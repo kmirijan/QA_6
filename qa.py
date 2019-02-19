f7e8bc2ad1ba916b8e7b009fec0825f8277bbb36
kmirijan <34608342+kmirijan@users.noreply.github.com>
2019-02-19 15:11:06 -0800

    Sentence Selector

--- qa.py
+++ qa.py
@@ -1,6 +1,55 @@
-
+import sys, nltk, operator
+from nltk.corpus import wordnet
 from qa_engine.base import QABase
 from qa_engine.score_answers import main as score_answers
+from nltk.stem.wordnet import WordNetLemmatizer
+
+stopwords = set(nltk.corpus.stopwords.words("english"))
+
+def get_wordnet_pos(treebank_tag):
+
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+
+def norm_question(question):
+    quest_words = nltk.word_tokenize(question)
+    quest_words = nltk.pos_tag(quest_words)
+    del quest_words[0]
+    del quest_words[-1]
+    lmtzr = WordNetLemmatizer()
+
+    root_question_words = set()
+    for word_pair in quest_words:
+        if word_pair[0] not in stopwords:
+            word = word_pair[0].lower()
+            tag = get_wordnet_pos(word_pair[1])
+            stem = lmtzr.lemmatize(word, tag)
+            root_question_words.add(stem)
+
+    return root_question_words
+
+def norm_text(sent):
+    sent_words = nltk.word_tokenize(sent)
+    sent_words = nltk.pos_tag(sent_words)
+    lmtzr = WordNetLemmatizer()
+
+    root_sent_words = set()
+    for word_pair in sent_words:
+        if word_pair[0] not in stopwords:
+            word = word_pair[0].lower()
+            tag = get_wordnet_pos(word_pair[1])
+            stem = lmtzr.lemmatize(word, tag)
+            root_sent_words.add(stem)
+
+    return root_sent_words
 
 
 def get_answer(question, story):
@@ -38,11 +87,33 @@ def get_answer(question, story):
     """
     ###     Your Code Goes Here         ###
 
-    answer = "whatever you think the answer is"
+    answers = []
 
+    boqw = norm_question(question['text'])
+    print(question['text'])
+    print(norm_question(question['text']))
 
-    ###     End of Your Code         ###
-    return answer
+    text = ''
+    if question['type'] == "Story":
+        text = story['text']
+    else:
+        text = story['sch']
+
+    sentences = nltk.sent_tokenize(text)
+    for sent in sentences:
+        # A list of all the word tokens in the sentence
+        bosw = norm_text(sent)
+        
+        # Count the # of overlapping words between the Q and the A
+        # & is the set intersection operator
+        overlap = len(boqw & bosw)
+        
+        answers.append((overlap, sent))
+    answers = sorted(answers, key=operator.itemgetter(0), reverse=True)
+
+    best_answer = (answers[0])[1]
+    print(best_answer)    
+    return best_answer
 
 
 
