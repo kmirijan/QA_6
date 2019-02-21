2b5a4c0d0a6265c6230b51f66453d24452c670b7
kmirijan <34608342+kmirijan@users.noreply.github.com>
2019-02-20 17:58:02 -0800

    Senetnce Selection done

--- qa.py
+++ qa.py
@@ -21,43 +21,71 @@ def get_wordnet_pos(treebank_tag):
         return wordnet.NOUN
 
 def norm_question(question):
-    quest_words = nltk.word_tokenize(question)
+    quest_words = nltk.word_tokenize(question['text'])
     quest_words = nltk.pos_tag(quest_words)
+    
     del quest_words[0]
     del quest_words[-1]
 
     root_question_words = set()
+    
+    # Adds the root word to troot_question_words
+    qgraph = question["dep"]
+    qword = find_main(qgraph)['lemma']
+    root_question_words.add(qword)
+    
     for word_pair in quest_words:
         if word_pair[0] not in stopwords:
             word = word_pair[0].lower()
-            tag = get_wordnet_pos(word_pair[1])
-            stem = lmtzr.lemmatize(word, tag)
-            root_question_words.add(stem)
+            tag  = get_wordnet_pos(word_pair[1])
+            
+            #Added in morphy function and added stems to root_question_words
+            stems = wordnet._morphy(word, tag)
+            [root_question_words.add(stem) for stem in stems]
+            if word_pair[1] == 'NNP':
+                root_question_words.add(word)
 
-    return root_question_words
+    return root_question_words, qword
 
 def norm_text(sent):
     sent_words = nltk.word_tokenize(sent)
     sent_words = nltk.pos_tag(sent_words)
 
     root_sent_words = set()
+
     for word_pair in sent_words:
+        
+        # If the word is a Verb, then add the word to root_sent_words
+        if (word_pair[1].startswith('V')):
+            [root_sent_words.add(word) for word in wordnet._morphy(word_pair[0].lower(), wordnet.VERB)]
+            continue
+
         if word_pair[0] not in stopwords:
             word = word_pair[0].lower()
-            tag = get_wordnet_pos(word_pair[1])
-            stem = lmtzr.lemmatize(word, tag)
-            root_sent_words.add(stem)
+            tag  = get_wordnet_pos(word_pair[1])
+            
+            #Added in morphy function and added stems to root_question_words
+            stems = wordnet._morphy(word, tag)
+            [root_sent_words.add(stem) for stem in stems]
+            if word_pair[1] == 'NNP':
+                root_sent_words.add(word)
 
     return root_sent_words
 
+def get_question_words(qgraph, qword):
+
+    qset = set()
+    for node in qgraph.nodes.values():
+        if node['word'] not in stopwords or node['word'] == qword:
+            if node['address'] != 0 and node['address'] != 1 and node['rel'] != 'punct':
+                qset.add(node['lemma'])
+    return qset
 
 def get_answer(question, story):
     """
     :param question: dict
     :param story: dict
     :return: str
-
-
     question is a dictionary with keys:
         dep -- A list of dependency graphs for the question sentence.
         par -- A list of constituency parses for the question sentence.
@@ -67,8 +95,6 @@ def get_answer(question, story):
         type -- whether you need to use the 'sch' or 'story' versions
                 of the .
         qid  --  The id of the question.
-
-
     story is a dictionary with keys:
         story_dep -- list of dependency graphs for each sentence of
                     the story version.
@@ -81,22 +107,18 @@ def get_answer(question, story):
         sch --  the raw text for the sch version.
         text -- the raw text for the story version.
         sid --  the story id
-
-
     """
     ###     Your Code Goes Here         ###
 
     answers = []
 
-    boqw = norm_question(question['text'])
-    qgraph = question["dep"]
-    qmain = find_main(qgraph)
-    qword = qmain["word"]
-    stem = lmtzr.lemmatize(qword, wordnet.VERB)
-    boqw.add(qword)
+    boqw, qword = norm_question(question)
     print(question['text'])
+    print(boqw)
+    print(qword)
 
     text = ''
+    
     if question['type'] == "Story":
         text = story['text']
     else:
@@ -114,14 +136,28 @@ def get_answer(question, story):
         answers.append((overlap, sent, bosw))
     answers = sorted(answers, key=operator.itemgetter(0), reverse=True)
 
-    best_answer = (answers[0])[1]
+    default_answer = (answers[0])[1]
+    max_overlap = (answers[0])[0]
+    best_answer = ''
+
+    for answer in answers:
+        if answer[0] == max_overlap and qword in answer[2]:
+            if best_answer == '':
+                best_answer = answer[1]
+            else:
+                best_answer = best_answer + ' ' + answer[1]
+
+    if best_answer == '':
+        best_answer = default_answer
+
+
+
     second_best_answer = (answers[1])[1]
-    bosw = (answers[0])[2]
-    print(boqw)
-    print(bosw)
+    print(question['qid'])
     print(best_answer)
     print(second_best_answer)
     print('\n')    
+
     return best_answer
 
 def find_main(graph):
@@ -138,7 +174,7 @@ def find_node(word, graph):
 
 def sent_test():
     driver = QABase()
-    q = driver.get_question("mc500.train.0.10")
+    q = driver.get_question("fables-02-4")
     story = driver.get_story(q["sid"])
 
     qgraph = q["dep"]
@@ -147,13 +183,13 @@ def sent_test():
     qset = set()
     for node in qgraph.nodes.values():
         if node['word'] not in stopwords or node['word'] == qword:
-            qset.add(node['lemma'])
-    print(qset)
-    print(len(qgraph))
+            if node['address'] != 0 and node['address'] != 1 and node['rel'] != 'punct':
+                qset.add(node['lemma'])
             
 
-    print(qword)
-    story_graphs = story["story_dep"]
+    sgraph = story["sch_dep"]
+    print(qgraph)
+    print(sgraph[1])
 
 
 #############################################################
@@ -175,11 +211,11 @@ def run_qa():
 
 
 def main():
-    sent_test()
-    #run_qa()
+    #sent_test()
+    run_qa()
     # You can uncomment this next line to evaluate your
     # answers, or you can run score_answers.py
-    #score_answers()
+    score_answers()
 
 if __name__ == "__main__":
     main()
